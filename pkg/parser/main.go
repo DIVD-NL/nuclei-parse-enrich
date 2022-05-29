9079c5aaeb5deecfe4e99605fabf9d27cc24159b
Pepijn van der Stap <85873966+xstp@users.noreply.github.com>
2022-05-29 13:42:22 +0200

    switch to json

--- pkg/parser/main.go
+++ pkg/parser/main.go
@@ -6,14 +6,11 @@ package parser
  */
 
 import (
-	"encoding/csv"
 	"encoding/json"
-	"fmt"
 	"io"
 	"nuclei-parse-enrich/pkg/enricher"
 	"nuclei-parse-enrich/pkg/types"
 	"os"
-	"reflect"
 
 	"github.com/sirupsen/logrus"
 )
@@ -24,7 +21,6 @@ type Parser struct {
 	Enrichment   []types.EnrichInfo
 	ScanRecords  []types.NucleiJsonRecord
 	MergeResults []types.MergeResult
-	OutputWriter *csv.Writer
 }
 
 func (p *Parser) NewParser(file *os.File) *Parser {
@@ -83,71 +79,17 @@ func (p *Parser) MergeScanEnrichment() {
 	logrus.Debug("parser: MergeScanEnrichment - merged ", len(p.MergeResults), " records")
 }
 
-func (p *Parser) WriteOutput() {
+func (p *Parser) WriteOutput(outputFile *os.File) {
 	logrus.Debug("parser: WriteOutput - start")
 
-	headerLine := []string{}
-
-	scanHeader := reflect.TypeOf(p.MergeResults[0].NucleiJsonRecord)
-	enrichHeader := reflect.TypeOf(p.MergeResults[0].EnrichInfo)
-
-	for i := 0; i < scanHeader.NumField(); i++ {
-		headerLine = append(headerLine, scanHeader.Field(i).Name)
+	flattened := make(map[string]interface{})
+	for _, mergeResult := range p.MergeResults {
+		flattened[mergeResult.EnrichInfo.Ip] = mergeResult
 	}
 
-	for i := 0; i < enrichHeader.NumField(); i++ {
-		if !contains(headerLine, enrichHeader.Field(i).Name) {
-			headerLine = append(headerLine, enrichHeader.Field(i).Name)
-		}
-	}
+	encoder := json.NewEncoder(outputFile)
+	encoder.SetIndent("", "  ")
+	encoder.Encode(flattened)
 
-	for i := 0; i < len(headerLine); i++ {
-		headerLine[i] = "'" + headerLine[i] + "'"
-	}
-
-	p.OutputWriter.Write(headerLine)
-
-	for _, record := range p.MergeResults {
-		line := []string{}
-
-		scanFields := reflect.ValueOf(record.NucleiJsonRecord)
-		enrichFields := reflect.ValueOf(record.EnrichInfo)
-
-		for i := 0; i < scanFields.NumField(); i++ {
-			if scanFields.Type().Field(i).Name == "MatcherStatus" {
-				line = append(line, fmt.Sprintf("%v", scanFields.Field(i).Interface()))
-			} else if scanFields.Type().Field(i).Name == "Info" {
-				info, err := json.Marshal(scanFields.Field(i).Interface())
-				if err != nil {
-					logrus.Debug(err)
-				}
-				line = append(line, string(info))
-			} else {
-				line = append(line, fmt.Sprintf("%v", scanFields.Field(i).Interface()))
-			}
-		}
-
-		for i := 0; i < enrichFields.NumField(); i++ {
-			if !contains(line, enrichFields.Field(i).String()) {
-				line = append(line, enrichFields.Field(i).String())
-			}
-		}
-
-		for i := 0; i < len(line); i++ {
-			line[i] = "'" + line[i] + "'"
-		}
-
-		p.OutputWriter.Write(line)
-	}
-
-	logrus.Debug("parser: WriteOutput - end")
-}
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
+	logrus.Debug("parser: WriteOutput - ended")
 }
