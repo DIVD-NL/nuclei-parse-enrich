b2995382f5c43b6e73c7202c100fd486d6188273
Martin van Wingerden <martin@martinvw.nl>
2023-02-24 15:12:50 +0100

    Corrected RipeSTAT structure of messages possible related to https://github.com/DIVD-NL/nuclei-parse-enrich/issues/6

--- pkg/ripestat/endpoints.go
+++ pkg/ripestat/endpoints.go
@@ -7,18 +7,18 @@ package ripestat
  */
 
 type ResponseBase struct {
-	Messages       []string `json:"messages"`
-	SeeAlso        []string `json:"see_also"`
-	DataCallName   string   `json:"data_call_name"`
-	DataCallStatus string   `json:"data_call_status"`
-	Cached         bool     `json:"cached"`
-	QueryID        string   `json:"query_id"`
-	ProcessTime    int      `json:"process_time"`
-	ServerID       string   `json:"server_id"`
-	BuildVersion   string   `json:"build_version"`
-	Status         string   `json:"status"`
-	StatusCode     int      `json:"status_code"`
-	Time           string   `json:"time"`
+	Messages       [][]string `json:"messages"`
+	SeeAlso        []string   `json:"see_also"`
+	DataCallName   string     `json:"data_call_name"`
+	DataCallStatus string     `json:"data_call_status"`
+	Cached         bool       `json:"cached"`
+	QueryID        string     `json:"query_id"`
+	ProcessTime    int        `json:"process_time"`
+	ServerID       string     `json:"server_id"`
+	BuildVersion   string     `json:"build_version"`
+	Status         string     `json:"status"`
+	StatusCode     int        `json:"status_code"`
+	Time           string     `json:"time"`
 }
 
 type AbuseContactFinderBase struct {
